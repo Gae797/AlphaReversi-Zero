21e72d88626dbed09f1e6e464f81970af65960fb
Gae797 <gae797@yahoo.it>
2022-08-19 22:26:54 +0200

    Human agent added

--- evaluate.py
+++ evaluate.py
@@ -4,12 +4,15 @@ from src.rl.config import *
 from src.evaluation.match import Match
 from src.agents.alpha_reversi_agent import AlphaReversiAgent
 from src.agents.random_agent import RandomAgent
+from src.agents.human_agent import HumanAgent
 
-weights_1 = os.path.join(WEIGHTS_PATH, "Generation {}".format(36), "variables")
-weights_2 = os.path.join(WEIGHTS_PATH, "Generation {}".format(10), "variables")
+weights_1 = os.path.join(WEIGHTS_PATH, "Generation {}".format(16), "variables")
+weights_2 = os.path.join(WEIGHTS_PATH, "Generation {}".format(24), "variables")
 
-agent_1 = AlphaReversiAgent(BOARD_SIZE, N_RESIDUAL_BLOCKS, weights_1, 400, name="Generation 36")
-agent_2 = AlphaReversiAgent(BOARD_SIZE, N_RESIDUAL_BLOCKS, weights_2, 400, name="Generation 10")
+agent_1 = AlphaReversiAgent(BOARD_SIZE, N_RESIDUAL_BLOCKS, weights_1, 200, name="Generation 16")
+agent_2 = AlphaReversiAgent(BOARD_SIZE, N_RESIDUAL_BLOCKS, weights_2, 200, name="Generation 24")
+agent_rnd = RandomAgent()
+agent_human = HumanAgent()
 
-match = Match(agent_1, agent_2, 10)
+match = Match(agent_2, agent_human, 2)
 match.play()
---
+++ src/agents/human_agent.py
@@ -0,0 +1,43 @@
+from src.agents.agent_interface import AgentInterface
+import src.environment.coordinate_handler as coordinate_handler
+
+from threading import Thread
+
+class HumanAgent(AgentInterface):
+
+    def __init__(self, name="Human agent"):
+
+        self.name = name
+
+    def play(self, board):
+
+        thread_result = []
+        thread = Thread(target=self.play_thread,args=(board, thread_result))
+
+        thread.start()
+        thread.join()
+
+        move = thread_result[0]
+
+        return move
+
+    def play_thread(self, board, thread_result):
+
+        legal_moves = board.legal_moves["indices"]
+
+        coordinates = [coordinate_handler.move_to_coordinate(move) for move in legal_moves]
+        print(coordinates)
+
+        coordinate = input("Choose a move: ")
+        coordinate = coordinate.lower()
+
+        if not coordinate in coordinates:
+            raise "Invalid move!"
+
+        result = coordinate_handler.coordinate_to_move(coordinate)
+
+        thread_result.append(result)
+
+    @property
+    def is_external_engine(self):
+        return False
